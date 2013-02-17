0cc59f8ae35d171b39e55b0a563490a1fdc6bb26
comatose <boncheol.gu@gmail.com>
2013-02-17 15:08:43 +0900

    * drafts/haksup/src/Haksup/Clustering/KMeans.hs:

--- drafts/haksup/src/Haksup/Clustering/KMeans.hs
+++ drafts/haksup/src/Haksup/Clustering/KMeans.hs
@@ -7,7 +7,6 @@ module Haksup.Clustering.KMeans
 
 -- import Debug.Trace
 import qualified Data.Map as M
-import Control.Arrow ((&&&))
 import Control.Parallel.Strategies
 import Data.List (foldl1', minimumBy)
 import Data.Function (on)
@@ -16,11 +15,11 @@ import Control.Monad (liftM2)
 type Point = [Double]
 
 cluster :: (a -> Point) -> [Point] -> [a] -> [[a]]
-cluster f cs = M.elems . M.fromListWith (++) . map (\x -> (closest x, [x]))
--- cluster cs = M.elems . M.fromListWith (++) . withStrategy (parListChunk 2500 rdeepseq) . (map (\x -> (closest x, [x])))
+cluster f cs = M.elems . M.fromListWith (++) . map (\x -> (closestOf x, [x]))
+-- cluster cs = M.elems . M.fromListWith (++) . withStrategy (parListChunk 2500 rdeepseq) . (map (\x -> (closestOf x, [x])))
   where
-    closest x = snd . minimumBy (compare `on` fst) . map (distance x &&& id) $ cs
-    distance x y = sum . (map (^2)) $ zipWith (-) (f x) y
+    closestOf x = minimumBy (compare `on` distance (f x)) cs
+    distance x y = sum . (map (^2)) $ zipWith (-) x y
 
 centroidOf :: [Point] -> Point
 centroidOf = liftM2 (divide) (foldl1' plus) (fromIntegral . length)
