a953236ffd1f32b3a85051fbb7b67b8dade86a18
comatose <boncheol.gu@gmail.com>
2013-02-17 14:58:36 +0900

    * drafts/haksup/src/Haksup/Clustering/KMeans.hs: Added generalized version of kmeans

--- drafts/Dude.hs
+++ drafts/Dude.hs
@@ -2,16 +2,11 @@ import System.Environment
 import qualified DedupeFFI as D
 import qualified Haksup.Clustering.KMeans as C
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as Bl
 import Data.Char
 import Data.List
 import Control.Monad
-
-instance C.Coordinate B.ByteString where
-  distance xs ys = sum . (map (^2)) $ zipWith (-) xs ys
-  centroid = liftM2 (divide) (B.foldl1' plus) (fromIntegral . B.length)
-    where
-      plus = zipWith (+)
-      divide xs n = map (/n) (map fromIntegral xs)
+import Data.Serialize
 
 main :: IO ()
 main = do
--- drafts/haksup/src/Haksup/Clustering/KMeans.hs
+++ drafts/haksup/src/Haksup/Clustering/KMeans.hs
@@ -1,29 +1,38 @@
-module Haksup.Clustering.KMeans where
+module Haksup.Clustering.KMeans
+       (
+         kmeansGen
+         , kmeans
+       )
+       where
 
 -- import Debug.Trace
 import qualified Data.Map as M
 import Control.Arrow ((&&&))
 import Control.Parallel.Strategies
-import Data.List (foldl1')
+import Data.List (foldl1', minimumBy)
+import Data.Function (on)
 import Control.Monad (liftM2)
 
 type Point = [Double]
 
-cluster :: [Point] -> [Point] -> [[Point]]
-cluster cs = M.elems . M.fromListWith (++) . map (\x -> (closest x, [x]))
+cluster :: (a -> Point) -> [Point] -> [a] -> [[a]]
+cluster f cs = M.elems . M.fromListWith (++) . map (\x -> (closest x, [x]))
 -- cluster cs = M.elems . M.fromListWith (++) . withStrategy (parListChunk 2500 rdeepseq) . (map (\x -> (closest x, [x])))
-  where closest x = snd . minimum . map (distance x &&& id) $ cs
-        distance xs ys = sum . (map (^2)) $ zipWith (-) xs ys
+  where
+    closest x = snd . minimumBy (compare `on` fst) . map (distance x &&& id) $ cs
+    distance x y = sum . (map (^2)) $ zipWith (-) (f x) y
+
+centroidOf :: [Point] -> Point
+centroidOf = liftM2 (divide) (foldl1' plus) (fromIntegral . length)
+  where plus x y = zipWith (+) x y
+        divide x n = map (/n) x
                     
+kmeansGen :: (a -> Point) -> Int -> [a] -> [[a]]
+kmeansGen f k xs = loop (take k . map f $ xs)
+  where loop cs = let clst = cluster f cs xs
+                      ncs = map (centroidOf . map f) clst
+                    -- ncs = withStrategy (parList rdeepseq) (map (centroidOf . map f) clst)
+                  in if cs == ncs then clst else loop ncs
 
 kmeans :: Int -> [Point] -> [[Point]]
-kmeans n xs = go (take n xs)
-  where go cs = let clst = cluster cs xs
-                    ncs = map centroid clst
-                    -- ncs = withStrategy (parList rdeepseq) (map centroid clst)
-                in if ncs == cs
-                   then clst
-                   else go ncs
-        centroid = liftM2 (divide) (foldl1' plus) (fromIntegral . length)
-        plus = zipWith (+)
-        divide xs n = map (/n) xs
+kmeans = kmeansGen id
