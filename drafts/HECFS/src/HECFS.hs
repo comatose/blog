8f74051bb49eab9c34b10f9e759ee154216f51fa
comatose <comatose>
2013-02-27 10:18:18 +0900

    * drafts/HECFS/src/HECFS.hs: cleaned up 'encode' and 'decode', removed customized 'Block'

--- drafts/HECFS/src/HECFS.hs
+++ drafts/HECFS/src/HECFS.hs
@@ -1,43 +1,17 @@
-{-# LANGUAGE DeriveGeneric #-}
 module HECFS where
 
 import           Codec.Encryption.Blowfish (decrypt, encrypt)
 import           Codec.Encryption.Padding  (pkcs5, unPkcs5)
+import           Codec.Utils               (listToOctets, listFromOctets)
 import qualified Codec.FEC                 as F
 import           Control.Monad
-import           Data.Bits
 import qualified Data.ByteString           as B
-import qualified Data.ByteString.Lazy      as Bl
-import qualified Data.Serialize            as S (Serialize, decode, encode)
-import           Data.Word
-import           GHC.Generics
-
-data Block = Block {getHeader :: Word8, getBody :: B.ByteString} deriving (Show, Generic)
-
-instance S.Serialize Block
-
-padBit = bitSize (undefined :: Word8) - 1
-
-isPadded :: Block -> Bool
-isPadded (Block b _) = testBit b padBit
-
-index :: Block -> Word8
-index (Block b _) = clearBit b padBit
-
-maxIndex :: Word8
-maxIndex = (clearBit maxBound padBit)
-
-makeBlock :: Bool -> Word8 -> B.ByteString -> Block
-makeBlock padded index body
-  | index > maxIndex = error $ "index should be <= " ++ show (maxIndex)
-  | padded = Block (setBit index padBit) body
-  | not padded = Block index body
 
 encode :: Integral a => a -> Int -> Int -> B.ByteString -> [B.ByteString]
-encode key k n = F.enFEC k n . S.encode . map (encrypt key) . pkcs5 . B.unpack
+encode key k n = F.enFEC k n . B.pack . listToOctets . map (encrypt key) . pkcs5 . B.unpack
 
 decode :: Integral a => a -> Int -> Int -> [B.ByteString] -> B.ByteString
-decode key k n = B.pack . unPkcs5 . map (decrypt key) . either error id . S.decode . F.deFEC k n
+decode key k n = B.pack . unPkcs5 . map (decrypt key) . listFromOctets . B.unpack . F.deFEC k n
 
 store :: Integral a => a -> FilePath -> Int -> [FilePath] -> IO ()
 store key src k trgs =
